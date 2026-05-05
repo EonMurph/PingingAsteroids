fbdae8e812553e803ff99296e0ee8ec68b5dc6f2
Eoghan Murphy <stiger.home@gmail.com>
2026-05-05 22:20:04 +0100

    Add state struct

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,11 @@ const rl = @import("raylib");
 const window = @import("window.zig").window;
 const Paddle = @import("paddle.zig").Paddle;
 
+const State = struct {
+    p1: *Paddle,
+    p2: *Paddle,
+};
+
 fn update(p1: *Paddle, p2: *Paddle) void {
     _ = p2;
     if (rl.isKeyDown(.w)) {
@@ -11,15 +16,21 @@ fn update(p1: *Paddle, p2: *Paddle) void {
     }
 }
 
-fn render(p1: *Paddle, p2: *Paddle) void {
-    p1.draw();
-    p2.draw();
+fn render(state: *State) void {
+    state.p1.draw();
+    state.p2.draw();
 }
 
 pub fn main() void {
+    var state: State = undefined;
+
     var p1 = Paddle.init(10);
     var p2 = Paddle.init(window.width - @as(u32, 10 + p1.width));
 
+    state = State{
+        .p1 = p1,
+        .p2 = p2,
+    };
     rl.initWindow(window.width, window.height, "Pong");
     defer rl.closeWindow();
 
